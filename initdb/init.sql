80fdde82167351b5317a1e97744d3d4983b07f25
Miguel Angel Vigo Bazm <vigobaz@hotmail.com>
2025-07-01 16:06:27 +0200

    merge contractApplication branch

--- initdb/init.sql
+++ initdb/init.sql
@@ -17,6 +17,25 @@ CREATE TABLE IF NOT EXISTS ports (
     longitude FLOAT NOT NULL
 );
 
+CREATE TABLE contracts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    forecast_system_id INTEGER NOT NULL REFERENCES forecast_systems(id) ON DELETE CASCADE,
+    start_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    end_date DATE,
+    active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+-- Tabla intermedia entre users y contracts
+CREATE TABLE user_contracts (
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    contract_id INTEGER NOT NULL REFERENCES contracts(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, contract_id)
+);
+
+
+
+
 -- Insertar usuario admin
 INSERT INTO users (username, email, hashed_password, is_admin, is_employee)
 VALUES ('admin', 'admin@example.com', '$2b$12$vBwE85h4womp.Ye8NoJOJe9LCWnwURGiFJSFI2VhhrnXIUDPdqn32', TRUE, TRUE)
@@ -33,3 +52,19 @@ INSERT INTO ports (name, country, latitude, longitude) VALUES
 ('Puerto de Róterdam', 'Países Bajos', 51.9225, 4.4792),
 ('Puerto de Hamburgo', 'Alemania', 53.5511, 9.9937)
 ON CONFLICT DO NOTHING;
+
+
+
+INSERT INTO contracts (name, forecast_system_id, start_date, end_date, active)
+VALUES 
+  ('Contrato A', 1, '2025-06-01', NULL, true),
+  ('Contrato B', 1, '2025-07-01', '2025-12-31', true),
+  ('Contrato C', 2, '2025-01-15', NULL, false);
+
+
+  INSERT INTO user_contracts (user_id, contract_id)
+VALUES 
+  (1, 1),
+  (1, 2),
+  (2, 2),
+  (3, 3);
